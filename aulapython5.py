f0e6582a675b3b433fef085ee3a33e9de5b9145b
MLKmalu <marialuisa.ctism@gmail.com>
2023-05-26 16:47:32 -0300

    aulinha

---
+++ aulapython5.py
@@ -0,0 +1,12 @@
+'''Classe Objeto Atributo
+
+
+'''
+
+class Pokemon():
+	"""docstring for Pokemon"""
+	def __init__(self, tipo, nome):
+		self.tipo=tipo
+		self.nome=nome
+charmander = Pokemon("fogo","charmander")
+
